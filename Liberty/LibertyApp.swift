e2846a8bd06f5beefbca738cce7f4a60fea6047a
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-21 13:03:50 -0500

    Initial Commit

---
+++ Liberty/LibertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibertyApp.swift
+//  Liberty
+//
+//  Created by J. DeWeese on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LibertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
